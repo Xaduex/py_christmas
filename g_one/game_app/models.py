9071ce37b00737b2205480e29d7603b578aa1ba2
Xaduex <1287389168746123@protonmail.com>
2020-12-22 11:10:46 +0100

    002

--- g_one/game_app/models.py
+++ g_one/game_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    description = models.TextField(blank=True)
+    short_description = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.short_description
